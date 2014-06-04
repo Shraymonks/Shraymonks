2a31c6d45848aa040071ae56e9a465145e3ff385
Raymond Ha <raymond@shraymonks.com>
2014-06-04 01:19:20 -0400

    Update analytics snippet

--- js/analytics.js
+++ js/analytics.js
@@ -1,20 +1,18 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-9190667-5']);
-_gaq.push(['_trackPageview']);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ga('create', 'UA-9190667-5', 'shraymonks.com');
+ga('send', 'pageview');
 
+(function () {
 	var links = document.getElementsByTagName('a');
-	var trackClick = function (title) {
-		return function () {
-			_gaq.push(['_trackEvent', 'Click', title]);
-		};
+	var trackClick = function (e) {
+		ga('send', 'event', 'link', 'click', e.target.href);
 	};
 
 	for (var i = 0; i < links.length; i++) {
-		links[i].onclick = trackClick(links[i].innerText);
+		links[i].addEventListener('click', trackClick);
 	}
-}).call(this);
+}());
